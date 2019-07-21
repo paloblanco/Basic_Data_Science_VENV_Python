db2b722fb683293f18793236a243f3306a94400f
paloblanco <45581243+paloblanco@users.noreply.github.com>
2019-07-21 11:41:38 -0700

    worked on scraping, solved 1

--- homework/hw1/RoccoHW1.py
+++ homework/hw1/RoccoHW1.py
@@ -1,13 +1,18 @@
 #%% [markdown]
 # # HW 1: Done by Rocco Panella
 # I am writing my homeworkd for CMUs data science courses in flat python files. I plan on doing this for two reasons:
+# 
 # 1. Get used to using a proper text editor for data science work
 # 2. I want to use TDD as I do this. I plan on writing a test file for every HW to test all of my funcitons.
 
 #%%
+from typing import Union, List, Dict, Any, Tuple
+
+
+#%%
 # Sample function - can I write a test?
 
-def add_args(*args):
+def add_args(*args: Union[int, float]) -> Union[int, float]:
     """
     For all added arguments, find the sum
     """
@@ -15,12 +20,9 @@ def add_args(*args):
     return my_sum
 
 #%%
-# rotate(l,n) takes a list l and an integer n, and returns a new list with the first n elements moved to the end. 
-#
-# Example: 
-#   rotate([1,2,3,4], 3) -> [4,1,2,3]
+# Getting Started Problems
 
-def rotate_list(l, n):
+def rotate_list(l: List[Any], n: int) -> List[Any]:
     """
     rotate(l,n) takes a list l and an integer n, and returns a new list with the first n elements moved to the end. 
     Example: rotate([1,2,3,4], 3) -> [4,1,2,3]
@@ -31,14 +33,43 @@ def rotate_list(l, n):
     list_solution = new_beginning + new_end
     return list_solution
     
-# reverse_dict(d) takes a dictionary d, and returns a new dictionary with the keys and values swapped.
-# Assume all values of the given dictionary are unique, i.e. don't worry about conflicting keys. 
-#
-# Example: 
-#   reverse_dict({"apple" : "red", "banana" : "yellow"}) -> {"red" : "apple", "yellow" : "banana"}
-def reverse_dict(d):
-    # Write solution here
-    pass
 
+def reverse_dict(d: Dict[Any, Any]) -> Dict[Any, Any]:
+    """
+    reverse_dict(d) takes a dictionary d, and 
+    returns a new dictionary with the keys and values swapped.
+    Assume all values of the given dictionary are unique, 
+    i.e. don't worry about conflicting keys. 
+    Example: 
+    reverse_dict({"apple" : "red", "banana" : "yellow"}) -> {"red" : "apple", "yellow" : "banana"}
+    """
+    new_dict: Dict[Any, Any] = {}
+    for key, val in d.items():
+        new_dict[val] = key
+    return new_dict
+
+
+#%%
+#========== Scraper Imports
+import io, time, json
+import requests
+from bs4 import BeautifulSoup
+
+#%%
+#============ Scraper Problems
+def retrieve_html(url: str) -> Tuple[int, str]:
+    """
+    Return the raw HTML at the specified URL.
+    Args:
+        url (string): 
+    Returns:
+        status_code (integer):
+        raw_html (string): the raw HTML content of the response, properly encoded according to the HTTP headers.
+    """
+    response = requests.get(url)
+    status_code = response.status_code
+    raw_html = response.text
+    
+    return status_code, raw_html
 
 #%%
--- homework/hw1/test_hw1.py
+++ homework/hw1/test_hw1.py
@@ -11,6 +11,43 @@ def test_rotate_list():
     """
     test_list = [1,2,3,4]
     test_num = 3
-    for i,j in zip(RoccoHW1.rotate_list(test_list, test_num), [4,1,2,3]):
+    test_result = RoccoHW1.rotate_list(test_list, test_num)
+    assert isinstance(test_result, list)
+    for i,j in zip(test_result, [4,1,2,3]):
         assert i == j
+    return
+
+
+def test_reverse_dict():
+    """
+    Should Reverse the order of a provided dict. should not be expected to handle duplicates
+    """
+    test_dict = {"apple" : "red", "banana" : "yellow"}
+    expected_result = {"red" : "apple", "yellow" : "banana"}
+    test_result = RoccoHW1.reverse_dict(test_dict)
+
+    assert isinstance(test_result, dict)
+    for key, val in expected_result.items():
+        assert val == test_result.get(key)
+    return
+
+def test_retrieve_html():
+    """
+    Return the raw HTML at the specified URL.
+    Args:
+        url (string): 
+    Returns:
+        status_code (integer):
+        raw_html (string): the raw HTML content of the response, properly encoded according to the HTTP headers.
+    """
+    test_url = 'http://www.nytimes.com/2016/08/28/magazine/inside-facebooks-totally-insane-unintentionally-gigantic-hyperpartisan-political-media-machine.html'
+    desired_code = 200
+    desired_html = u'<!DOCTYPE html>\n<!--[if (gt IE 9)|!(IE)]> <!--> <html lang="en" class="no-js section-magazine...'
+
+    test_code, test_html = RoccoHW1.retrieve_html(test_url)
+
+    assert isinstance(test_code, int)
+    assert isinstance(test_html, str) # this is adequate to know we are returning html
+    assert test_code == desired_code
+    # assert test_html[:20] == desired_html[:20]
     return

e23fd475aa03822fa9583480680ec58ddc1077ab
Rocco Panella <45581243+paloblanco@users.noreply.github.com>
2023-11-13 09:03:21 -0500

    cleaned up notebooks

--- bootstrap_python_venv.bat
+++ bootstrap_python_venv.bat
@@ -1,3 +1,7 @@
-echo You need to have python 3.6+ installed and added to you path
+echo You need to have python 3.6+ installed and added to your path
+echo Creating a virtual environment...
 python -m venv venv
+echo OK
+echo Installing Requirements...
 call install_requirements.bat
+echo DONE
